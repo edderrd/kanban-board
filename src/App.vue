e6c865d7a74edd00c5f8c6b9890c4c50551a3741
Edder Rojas <edder.rojas@gmail.com>
2020-04-08 17:48:31 -0600

    Cleaned up boilerplate

--- src/App.vue
+++ src/App.vue
@@ -4,53 +4,32 @@
       app
       color="primary"
       dark
+      dense
     >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
+      <v-toolbar-title>Kanban Board</v-toolbar-title>
       <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
       </v-btn>
     </v-app-bar>
 
     <v-content>
-      <HelloWorld/>
+      <Board />
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import Board from './components/Board.vue';
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
+    Board,
   },
 
   data: () => ({
---
+++ src/components/Board.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>Board</h1>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
